e3b869d9416fbe12a58f900f77d5c10ccadad1e4
comahead <comahead@gmail.com>
2018-07-27 00:04:13 +0900

    .

---
+++ src/collection.js
@@ -0,0 +1,16 @@
+((core) => {
+    /*var Collection = core.BaseClass.extend({
+        initialize() {
+            var args = [].slice.call(arguments);
+
+            core.each(args, (item, i) => {
+                this[i] = item;
+            });
+        }
+    });
+
+    var fn = Collection.prototype.constructor = [];*/
+
+    var Coll3ection = Array;
+
+})(window.Grim);
