896fcf542d929046c5103a8d2a16e5b29cffac00
Gilbert <hanulo78@gmail.com>
2018-07-26 21:24:14 +0900

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractSass = new ExtractTextPlugin('css/style.css');
+
+module.exports = {
+    entry: {app: path.resolve(__dirname, "src/main.ts")},
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["es2015"]
+                    }
+                }
+            },
+            {
+                test: /\.ts$/,
+                use: ['ts-loader']
+            },
+            {
+                test: /\.(s*)css$/,
+                use: extractSass.extract(["css-loader", "sass-loader"])
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/index.html'),
+            inject: true,
+            filename: path.join(__dirname, 'dist/index.html')
+        }),
+        extractSass
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }
+};
